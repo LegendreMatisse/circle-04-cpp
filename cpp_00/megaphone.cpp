75aea9dd52b5db2b217484c58ee4481d60f2cd4c
LegendreMatisse <legendre.matisse@gmail.com>
2024-07-24 01:50:44 +0200

    added some more comments with more info and added an std:: to a function call

--- cpp_00/megaphone.cpp
+++ cpp_00/megaphone.cpp
@@ -1,4 +1,14 @@
-/*header pls*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>              +#+  +:+       +#+   */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 01:35:04 by mlegendr            #+#    #+#           */
+/*   Updated: 2024/07/24 01:35:04 by mlegendr           ###   ########.fr     */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
 
@@ -11,9 +21,22 @@ int main(int argc, char **argv)
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
-				std::cout << static_cast<char>(toupper(argv[i][j])); //using a static_cast cast the output of toupper to a char, it's safer than using a C-style cast, or so I've been told
+			{
+				/*
+					Using a static_cast cast the output of toupper to a char.
+					It's safer than using a C-style cast, or so I've been told.
+				*/
+				/*
+					Adding std:: to the toupper function call will make
+					the code more consistent and clear. Especially when
+					mixing C and C++ functions.
+					This seems to be a good practice to follow, 
+					or so I've been told...
+				*/
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+			}
 		}
-		//prints a new line at the end of the output
+		//Prints a new line at the end of the output.
 		std::cout << std::endl;
 	}
 	return 0;
