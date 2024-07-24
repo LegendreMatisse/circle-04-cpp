4c1e5024d3503fd7906c40d9296330434050e915
Matisse Legendre <legendre.matisse@gmail.com>
2024-07-24 20:47:24 +0200

    Progress Wed 24 Jul 2024 08:47:24 PM CEST

--- cpp_01/Contact.hpp
+++ cpp_01/Contact.hpp
@@ -6,8 +6,25 @@
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 18:00:30 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/07/24 18:00:31 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/07/24 20:46:55 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
+# include <string> // ==> used for the string class
+# include <iostream>
+# include <iomanip> // ==> used for std::setw()
+
+class Contact
+{
+	private:
+			Contact(void); //constructor
+			~Contact(void); //destructor
+
+			//getters
+			std::string GetFirstName()			
+}
+
+#endif
