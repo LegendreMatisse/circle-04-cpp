235dd3284475814adcfdc7c1fc01e8f054b4251a
LegendreMatisse <legendre.matisse@gmail.com>
2024-07-25 02:09:21 +0200

    test to see if the getters and setters work

--- cpp_01/Contact.hpp
+++ cpp_01/Contact.hpp
@@ -24,7 +24,55 @@ class Contact
 			~Contact(void); //destructor
 
 			//getters
-			std::string GetFirstName()			
+			std::string GetFirstName() const
+			{
+				return _FirstName;
+			}
+			std::string GetLastName() const
+			{
+				return _SecondName;
+			}
+			std::string GetNickname() const
+			{
+				return _Nickname;
+			}
+			std::string GetPhoneNumber() const
+			{
+				return _PhoneNumber;
+			}
+			std::string GetDarkestSecret() const
+			{
+				return _DarkestSecret;
+			}
+
+			//setters
+			void SetFirstName(std::string &first_name)
+			{
+				this->_FirstName = first_name;
+			}
+			void SetLastName(std::string &last_name)
+			{
+				this->_SecondName = last_name;
+			}
+			void SetNickname(std::string &nickname)
+			{
+				this->_Nickname = nickname;
+			}
+			void SetPhoneNumber(std::string &phone_number)
+			{
+				this->_PhoneNumber = phone_number;
+			}
+			void SetDarkestSecret(std::string &darkest_secret)
+			{
+				this->_DarkestSecret = darkest
+			}
+
+	private:
+			std::string _FirstName;
+			std::string _SecondName;
+			std::string _Nickname;
+			std::string _PhoneNumber;
+			std::string _DarkestSecret;
 }
 
 #endif
--- cpp_01/main.cpp
+++ cpp_01/main.cpp
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+int main(void)
+{
+	Contact contact;
+
+	std::string first_name = "John";
+	std::string last_name = "Doe";
+	std::string nickname = "JD";
+	std::string phone_number = "123456789";
+	std::string darkest_secret = "I'm Batman";
+
+	contact.SetFirstName(first_name);
+	contact.SetLastName(last_name);
+	contact.SetNickname(nickname);
+	contact.SetPhoneNumber(phone_number);
+	
+	std::cout << "First name: " << contact.GetFirstName() << std::endl;
+	std::cout << "Last name: " << contact.GetLastName() << std::endl;
+	std::cout << "Nickname: " << contact.GetNickname() << std::endl;
+	std::cout << "Phone number: " << contact.GetPhoneNumber() << std::endl;
+}
