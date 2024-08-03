cc9f3993a1f067eebdec749fdb4b46eab8329d27
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-04 00:09:43 +0200

    Update ZombieHorde.cpp

--- cpp_01/ex01/ZombieHorde.cpp
+++ cpp_01/ex01/ZombieHorde.cpp
@@ -14,10 +14,17 @@
 
 Zombie	*zombieHorde(int n, std::string name)
 {
-	Zombie	*horde = new Zombie[n];
+	Zombie horde[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		horde[i] = Zombie(name);
+	}
+	return horde;
+	/*Zombie	*horde = new Zombie[n];
 	for (int i = 0; i < n; i++)
 	{
 		horde[i].setName(name);
 	}
-	return horde;
+	return horde;*/
 }
