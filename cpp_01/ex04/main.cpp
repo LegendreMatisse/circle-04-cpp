3ea2a65928951e317bacc79cde2ed701c07f01d4
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-04 22:35:02 +0200

    Started working on cpp01-ex04

---
+++ cpp_01/ex04/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 21:53:22 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/08/04 21:53:22 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "main.hpp"
+
+int main(int argv, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << USAGE_ERR << std::endl;
+		return (1);
+	}
+	else if (argv[1] == "" || argv[2] == "" || argv[3] == "")
+	{
+		std::cout << USAGE_ERR << std::endl;
+		return (1);
+	}
+}
---
+++ cpp_01/ex04/main.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 22:32:48 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/08/04 22:32:48 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <iostream>
+# include <fstream>
+# include <string>
+
+# define USAGE_ERR "Usage: ./replace [filename] [string to replace] [string to replace with]"
+# define FILE_ERR "Error: could not open file."
+
+#endif
