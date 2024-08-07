0659dd01df692b4bedc4fed8ed4f41c87116fb74
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-08 00:46:31 +0200

    testing cpp02-ex01

--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -10,3 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int					_fixedValue;
+		static const int	_fractionalBits = 8;
+	public:
+		//constructor
+		Fixed(void);
+
+		//copy constructor
+		Fixed(const Fixed &copyCo);
+
+		//assignation operator overload
+		Fixed &operator=(const Fixed &copyOp);
+
+		//parameterized constructor
+		Fixed(const int value);
+		Fixed(const float value);
+
+		//destructor
+		~Fixed();
+
+		//getter
+		int		getRawBits(void) const;
+
+		//setter
+		void	setRawBits(int const raw);
+
+		//functions
+		float	toFloat(void) const;
+		int		toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value);
+
+#endif
