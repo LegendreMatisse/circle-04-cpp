fad7d4020ccbd9fcb2c1e1e24d46c3a3df33e98a
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-09 20:58:54 +0200

    Update main.cpp

--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -18,9 +18,9 @@ int main(void)
 	Fixed b(39);
 	Fixed c(8);
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
 	std::cout << std::endl;
 
 	bool test = a > b;
