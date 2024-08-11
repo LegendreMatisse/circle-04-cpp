62a887ed07628e5514a12222504e0e66319ed721
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-11 17:31:56 +0200

    Setup for cpp03-ex01
    - Added all required files
    - Copied over the ClapTrap classes from the previous exercise (cpp03-ex00)
    - Added new files to the Makefile
    - Added a header to all files

---
+++ cpp_03/ex01/ClapTrap.cpp
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 17:29:10 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/08/11 17:29:10 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : _name("default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap parameterized constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copyCo)
+{
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+	*this = copyCo;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &copyOp)
+{
+	std::cout << "ClapTrap assignment operator called" << std::endl;
+	if (this != &copyOp)
+	{
+		this->_name = copyOp._name;
+		this->_hitPoints = copyOp._hitPoints;
+		this->_energyPoints = copyOp._energyPoints;
+		this->_attackDamage = copyOp._attackDamage;
+	}
+	return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+std::string ClapTrap::getName(void) const
+{
+	return (this->_name);
+}
+
+int ClapTrap::getHitPoints(void) const
+{
+	return (this->_hitPoints);
+}
+
+int ClapTrap::getEnergyPoints(void) const
+{
+	return (this->_energyPoints);
+}
+
+int ClapTrap::getAttackDamage(void) const
+{
+	return (this->_attackDamage);
+}
+
+void ClapTrap::setName(std::string name)
+{
+	this->_name = name;
+}
+
+void ClapTrap::setHitPoints(int hitPoints)
+{
+	this->_hitPoints = hitPoints;
+}
+
+void ClapTrap::setEnergyPoints(int energyPoints)
+{
+	this->_energyPoints = energyPoints;
+}
+
+void ClapTrap::setAttackDamage(int attackDamage)
+{
+	this->_attackDamage = attackDamage;
+}
+
+void ClapTrap::determineMessage(std::string const &message, std::string const &target, int amount)
+{
+	if (message == "attack")
+		std::cout << "ClapTrap " << ClapTrap::getName() << " attacks " << target << " causing, " << ClapTrap::getAttackDamage() << " points of damage!" << std::endl;
+	else if (message == "takeDamage")
+		std::cout << "ClapTrap " << ClapTrap::getName() << " takes " << amount << " points of damage!" << std::endl;
+	else if (message == "beRepaired")
+		std::cout << "ClapTrap " << ClapTrap::getName() << " is being repaired for " << amount << " points!" << std::endl;
+}
+
+void ClapTrap::attack(std::string const &target)
+{
+	determineMessage("attack", target, 0);
+	ClapTrap::setEnergyPoints(ClapTrap::getEnergyPoints() - 1);
+	return ;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	determineMessage("takeDamage", "himself", amount);
+	ClapTrap::setHitPoints(ClapTrap::getHitPoints() - amount);
+	return ;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	determineMessage("beRepaired", "himself", amount);
+	ClapTrap::setHitPoints(ClapTrap::getHitPoints() + amount);
+	ClapTrap::setEnergyPoints(ClapTrap::getEnergyPoints() - 1);
+	return ;
+}
+
+//debug functions
+void ClapTrap::printStats(void)
+{
+	std::cout << "Name: " << ClapTrap::getName() << std::endl;
+	std::cout << "Hitpoints: " << ClapTrap::getHitPoints() << std::endl;
+	std::cout << "Energy Points: " << ClapTrap::getEnergyPoints() << std::endl;
+	std::cout << "Attack Damage: " << ClapTrap::getAttackDamage() << std::endl;
+	return ;
+}
---
+++ cpp_03/ex01/ClapTrap.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 17:29:19 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/08/11 17:29:19 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+
+	public:
+		//constructor
+		ClapTrap(void);
+
+		//parameterized constructor
+		ClapTrap(std::string name);
+
+		//copy constructor
+		ClapTrap(const ClapTrap &copyCo);
+
+		//assignment operator overload
+		ClapTrap &operator=(const ClapTrap &copyOp);
+
+		//destructor
+		~ClapTrap();
+
+		//getters
+		std::string	getName(void) const;
+		int			getHitPoints(void) const;
+		int			getEnergyPoints(void) const;
+		int			getAttackDamage(void) const;
+
+		//setters
+		void		setName(std::string name);
+		void		setHitPoints(int hitpoints);
+		void		setEnergyPoints(int energyPoints);
+		void		setAttackDamage(int attackDamage);
+
+		//functions
+		void		attack(std::string const &target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		void		determineMessage(std::string const &message, std::string const &target, int amount);
+
+		//debug functions
+		void		printStats(void);
+};
+
+#endif
