a409647937c533a13d98fdb8d4c705982c9507fe
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-11 18:26:14 +0200

    Some work on ScavTrap

--- cpp_03/ex01/ScavTrap.hpp
+++ cpp_03/ex01/ScavTrap.hpp
@@ -10,3 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		//constructor
+		ScavTrap(void);
+
+		//parameterized constructor
+		ScavTrap(std::string const &name);
+
+		//copy constructor
+		ScavTrap(const ScavTrap &copyCo);
+
+		//assignment operator overload
+		ScavTrap &operator=(const ScavTrap &copyOp);
+
+		//destructor
+		~ScavTrap();
+
+		//functions
+		void		attack(std::string const &target);
+		void		guardGate(void);
+};
+
+#endif
