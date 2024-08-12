42ffef3b4ae3b927adb96a01606ff9fc78fc1678
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-13 00:49:32 +0200

    Added the WrongAnimal class
    - Added the WrongAnimal files
    - Added WrongAnimal.cpp to Makefile
    - Added 42 header sto the new files
    - Implemented the class and its fucntions
    - Added WrongAnimal to the main

---
+++ cpp_04/ex00/WrongAnimal.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongWrongAnimal.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 00:43:13 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/08/13 00:43:13 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongWrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(void)
+{
+	std::cout << "An WrongAnimal has been created" << std::endl;
+	this->setType("WrongAnimal");
+}
+
+WrongAnimal::WrongAnimal(WrongAnimal const &copyCo)
+{
+	std::cout << "An WrongAnimal has been created by copy" << std::endl;
+	*this = copyCo;
+}
+
+WrongAnimal &WrongAnimal::operator=(WrongAnimal const &copyOp)
+{
+	if (this == &copyOp)
+		return *this;
+	_type = copyOp._type;
+	std::cout << "An WrongAnimal has been assigned" << std::endl;
+	return *this;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+	std::cout << "An WrongAnimal has been destroyed" << std::endl;
+}
+
+std::string WrongAnimal::getType() const
+{
+	return _type;
+}
+
+void	WrongAnimal::setType(std::string const &type)
+{
+	this->_type = type;
+}
+
+void WrongAnimal::makeSound() const
+{
+	std::cout << "This WrongAnimal shrieks in a sound only audible to the Gods..." << std::endl;
+}
--- cpp_04/ex00/main.cpp
+++ cpp_04/ex00/main.cpp
@@ -13,6 +13,7 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
 
 int main(void)
 {
@@ -36,5 +37,21 @@ int main(void)
 	delete(j);
 	delete(i);
 
+	std::cout << "\nThe wrong animals are here..." << std::endl;
+	std::cout << "\nMay the Gods have mercy on our souls..." << std::endl;
+	const WrongAnimal* steve	= new WrongAnimal;
+
+	std::cout << "\nPrinting the types of the various wrong animals..." << std::endl;
+	std::cout << "meta: " << steve->getType() << std::endl;
+	std::cout << "j: " << karl->getType() << std::endl;
+
+	std::cout << "\nCalling the makeSound function for the various wrong animals..." << std::endl;
+	steve->makeSound();
+	karl->makeSound();
+
+	std::cout << "\nDeleting the various wrong animals..." << std::endl;
+	delete(steve);
+	delete(karl);
+	
 	return (0);
 }
