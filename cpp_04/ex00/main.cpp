4f7c1d512a037f3595946cd5283e3d3fca355a97
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-12 22:43:37 +0200

    refined debug messages in cpp04-ex00
    - Added some more debug messages to make the output easier to read

--- cpp_04/ex00/main.cpp
+++ cpp_04/ex00/main.cpp
@@ -15,17 +15,19 @@
 
 int main(void)
 {
-	std::cout << "Creating the various animals...\n" << std::endl;
+	std::cout << "Creating the various animals..." << std::endl;
 	const Animal* meta	= new Animal();
 	const Animal* j		= new Dog();
 
-	std::cout << "Printing the types of the various animals...\n" << std::endl;
+	std::cout << "\nPrinting the types of the various animals..." << std::endl;
 	std::cout << meta->getType() << std::endl;
 	std::cout << j->getType() << std::endl;
-	std::cout << std::endl;
+
+	std::cout << "\nCalling the makeSound function for the various animals..." << std::endl;
 	meta->makeSound();
 	j->makeSound();
 
+	std::cout << "\nDeleting the various animals..." << std::endl;
 	delete(meta);
 	delete(j);
 
