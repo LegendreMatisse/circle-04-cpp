1e5dea8aa27a5865e61af7b15f2521d350a2ae37
LegendreMatisse <legendre.matisse@gmail.com>
2024-08-18 00:37:47 +0200

    Worked on cpp05-ex00 and hopefully finished it
    - Implemented main that tests the code I wrote

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -10,3 +10,67 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	try
+	{
+		Bureaucrat *steven = new Bureaucrat("Steven", 19);
+		try
+		{
+			steven->incrementGrade();
+			steven->decrementGrade();
+		}
+		catch(const std::exception &e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << *steven;
+		delete steven;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat *steven = new Bureaucrat("Steven", 1);
+		try
+		{
+			steven->incrementGrade();
+			steven->decrementGrade();
+		}
+		catch(const std::exception &e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << *steven;
+		delete steven;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat *steven = new Bureaucrat("Steven", 100);
+		try
+		{
+			steven->incrementGrade();
+			steven->decrementGrade();
+		}
+		catch(const std::exception &e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << *steven;
+		delete steven;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
