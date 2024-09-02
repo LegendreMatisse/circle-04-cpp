128f7f04c5f2bbc79a82131c8ed98cc8138bbe21
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-02 17:22:06 +0200

    Update iter.hpp

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -16,9 +16,9 @@
 # include <iostream>
 
 template <typename Iter>
-void iter(Iter *arr, size_t len, void (*function)(const Iter &in))
+void iter(Iter *arr, int len, void (*function)(const Iter &in))
 {
-	for (size_t i = 0; i < len; i++)
+	for (int i = 0; i < len; i++)
 	{
 		function(arr[i]);
 	}
