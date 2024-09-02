0f01331cde49b71793fa7e9824dbad7d1b6fa2ec
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-02 17:46:48 +0200

    Done with cpp07

---
+++ cpp_07/ex02/Array.hpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 17:26:44 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/09/02 17:26:44 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+template <typename T>
+class Array
+{
+	private:
+		T				*_arr;
+		unsigned int	_size;
+	public:
+		//constructor
+		Array(void);
+
+		//parameterized constructor
+		Array(unsigend int n);
+
+		//copy constructor
+		Array(const Array &copyCo);
+
+		//copy assignment operator
+		Array &operator=(const Array &copyOp)
+
+		//destructor
+		~Array();
+
+		//operator overload
+		T &operator[](unsigned int index);
+};
+
+template <typename T>
+Array<T>::Array(void) : _arr(NULL), _size(0)
+{
+	std::cout << "An instance of Array was created." << std::endl;
+}
+Array<T>::Array(unsigned int n) : _arr(new T[n]), _size(n)
+{
+	std::cout << "An instance of Array was created with parameters." << std::endl;
+}
+
+Array<T>::Array(const Array &copyCo)
+{
+	*this = copyCo;
+	std::cout << "An instance of Array was created by copy." << std::endl;
+}
+
+Array<T> &Array<T>::operator=(const Array &copyOp)
+{
+	if (this == &copyOp)
+		return *this;
+	if (_arr)
+		delete[] _arr;
+	this->_size = copyOp._size;
+	this->_array = new T[this->_size];
+	for (unsigned int i = 0; i < this->_size; i++)
+	{
+		this->_arr[i] = copyOp._arr[i];
+	}
+	std::cout << "An instance of Array has been assigned." << std::endl;
+	return *this;
+}
+
+Array<T>::~Array()
+{
+	if (this->_arr)
+		delete[] _arr;
+	std::cout << "An instance of Array was destroyed." << std::endl;
+}
+
+Array<T> &Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_size)
+		throw std::out_of_range("The index has gone out of bounds.");
+	return this->_arr[index];
+}
+#endif
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -5,8 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/02 16:56:36 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/09/02 16:56:36 by mlegendr         ###   ########.fr       */
+/*   Created: 2024/09/02 17:26:50 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/09/02 17:26:51 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <Array.hpp>
+#include <ctime>
+#include <cstdlib>
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
