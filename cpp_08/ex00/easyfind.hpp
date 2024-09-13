48ddb5712d6e0be0b9ed43b2ec401cb62b348901
Matisse Legendre <legendre.matisse@gmail.com>
2024-09-13 16:30:14 +0200

    Progress Fri 13 Sep 2024 04:30:13 PM CEST

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/13 15:22:18 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/09/13 15:22:19 by mlegendr         ###   ########.fr       */
+/*   Created: 2024/09/13 15:22:37 by mlegendr          #+#    #+#             */
+/*   Updated: 2024/09/13 16:29:57 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <iostream>
+# include <exception>
+# include <vector>
+
+
+
+#endif
