f4949d9cb0b41e93ffbb51d9ccf37471dc931c35
Matisse Legendre <legendre.matisse@gmail.com>
2024-09-16 19:02:10 +0200

    Progress Mon 16 Sep 2024 07:02:10 PM CEST

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 15:22:37 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/09/13 16:29:57 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/09/16 18:13:34 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@
 # include <exception>
 # include <vector>
 
-
+template <typename T>
+typename T::const_iterator easyfind(const T &container, const int &value)
+{
+	if (container.empty())
+		throw std::out_of_range("Container is empty");
+	for (std::vector<int>::const_iterator it = container.begin(); it != container.end(); it++)	
+	{
+		if (*it == value)
+			return it;
+	}
+	throw std::runtime_error("Value not found.");
+}
 
 #endif
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,7 +6,30 @@
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 15:22:18 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/09/13 15:22:19 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/09/16 18:13:46 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main(void)
+{
+	std::vector<int> test;
+
+	test.push_back(10);
+    test.push_back(20);
+    test.push_back(30);
+
+	try
+	{
+		std::vector<int> test2;
+		std::vector<int>::const_iterator it = easyfind(test2, 20);
+		std::cout << *it << std::endl;	
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+	
+	return (0);
+}
