62842f6387dfc454d8840bb6aee3a9d32e2231dd
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-23 01:28:55 +0200

    Update main.cpp

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -14,63 +14,6 @@
 
 int main(void)
 {
-	std::vector<int> test;
-
-	test.push_back(6);
-	test.push_back(3);
-	test.push_back(17);
-	test.push_back(9);
-	test.push_back(11);
-
-	int lowestSpan = 0;
-	int highestSpan = 0;
-	int temp = 0;
-	
-	std::sort(test.begin(), test.end());
-
-	for (std::vector<int>::iterator it = test.begin(); it != test.end(); ++it)
-	{
-		for (std::vector<int>::iterator er = test.begin(); er != test.end(); ++er)
-		{
-			if (*it > *er && *it != *er)
-				temp = *it - *er;
-			else if (*it < *er && *it != *er)
-				temp = *er - *it;
-			if (lowestSpan == 0 && highestSpan == 0)
-				lowestSpan = temp;
-			if (temp < lowestSpan)
-				lowestSpan = temp;
-			if (temp > highestSpan)
-				highestSpan = temp;
-		}
-	}
-	
-	std::cout << lowestSpan << std::endl;
-	std::cout << highestSpan << std::endl;
-
-	lowestSpan = 0;
-	highestSpan = 0;
-
-	std::sort(test.begin(), test.end());
-
-	std::vector<int> differences(test.size());
-
-	std::adjacent_difference(test.begin(), test.end(), differences.begin());
-
-	for (size_t i = 1; i < differences.size(); ++i) {
-		if (differences[i] < lowestSpan && differences[i] > 0) {
-			lowestSpan = differences[i];
-		}
-		if (differences[i] > highestSpan) {
-			highestSpan = differences[i];
-		}
-	}
-
-	std::cout << lowestSpan << std::endl;
-	std::cout << highestSpan << std::endl;
-
-	std::cout << "--__--__--__--__--" << std::endl;
-
 	Span *spanTest1 = new Span(5);
 
 	spanTest1->addNumber(6);
@@ -86,7 +29,7 @@ int main(void)
 	}
 	catch(const std::exception &e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << e.what() << std::flush;
 	}
 
 	std::cout << "--__--__--__--__--" << std::endl;
@@ -100,7 +43,7 @@ int main(void)
 	}
 	catch(const std::exception &e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << e.what() << std::flush;
 	}
 
 	std::cout << "--__--__--__--__--" << std::endl;
@@ -116,7 +59,7 @@ int main(void)
 	}
 	catch(const std::exception &e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << e.what() << std::flush;
 	}
 
 	return (0);
