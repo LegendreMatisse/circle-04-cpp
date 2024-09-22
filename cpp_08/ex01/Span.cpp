dcda7e51040b709a013c661fc4024ce6dd6959a4
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-23 01:10:57 +0200

    Update Span.cpp

--- cpp_08/ex01/Span.cpp
+++ cpp_08/ex01/Span.cpp
@@ -58,11 +58,32 @@ std::vector<int> Span::prepVector() const
 		throw NotEnoughNumbersEx();
 
 	std::vector<int> temp(this->_spanVec);
+
+	for (std::vector<int>::iterator it = temp.begin(); it != temp.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+
 	std::sort(temp.begin(), temp.end());
+
+	for (std::vector<int>::iterator it = temp.begin(); it != temp.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
 
 	std::vector<int> span(temp.size());
 
+	for (std::vector<int>::iterator it = span.begin(); it != span.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+
 	std::adjacent_difference(temp.begin(), temp.end(), span.begin());
+
+	for (std::vector<int>::iterator it = temp.begin(); it != temp.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
 
 	return span;
 }
