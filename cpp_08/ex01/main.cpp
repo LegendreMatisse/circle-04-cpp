577e2bc569dfe7ac819d72bb6db082f87d692b5b
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-22 20:00:44 +0200

    Update main.cpp

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -14,6 +14,7 @@ int main(void)
 	test.push_back(11);
 
 	int lowestSpan = 0;
+	int highestSpan = 0;
 	int temp = 0;
 	
 	for (std::vector<int>::iterator it = test.begin(); it != test.end(); ++it)
@@ -24,14 +25,17 @@ int main(void)
 				temp = *it - *er;
 			else if (*it < *er && *it != *er)
 				temp = *er - *it;
-			/*if (lowestSpan == 0)
-				lowestSpan = temp;*/
+			if (lowestSpan == 0 && highestSpan == 0)
+				lowestSpan = temp;
 			if (temp < lowestSpan)
 				lowestSpan = temp;
+			if (temp > highestSpan)
+				highestSpan = temp;
 		}
 	}
 	
 	std::cout << lowestSpan << std::endl;
+	std::cout << highestSpan << std::endl;
 	
 	return (0);
 }
