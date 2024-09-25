ca623dbbb1e8c0b6b87b91e9dc8a1c27636d2e39
LegendreMatisse <legendre.matisse@gmail.com>
2024-09-25 17:54:43 +0200

    Update main.cpp

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -16,49 +16,66 @@
 int main(void)
 {
 	std::cout << "MutantStack tests" << std::endl;
+
 	MutantStack<int> mstack;
+
 	mstack.push(5);
 	mstack.push(17);
+
 	std::cout << mstack.top() << std::endl;
+
 	mstack.pop();
+
 	std::cout << mstack.size() << std::endl;
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+
 	++it;
 	--it;
+	
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
+
 	std::cout << "--__--__--__--" << std::endl;
 
 	std::cout << "List tests" << std::endl;
+
 	std::list<int> list_test;
+
 	list_test.push_back(5);
 	list_test.push_back(17);
-	std::cout << list_test.top() << std::endl;
+
+	std::cout << list_test.back() << std::endl;
+
 	list_test.pop();
+
 	std::cout << list_test.size() << std::endl;
+
 	list_test.push_back(3);
 	list_test.push_back(5);
 	list_test.push_back(737);
 	list_test.push_back(0);
+
 	std::list<int>::iterator it_list = list_test.begin();
 	std::list<int>::iterator ite_list = list_test.end();
+
 	++it_list;
 	--it_list;
+
 	while (it_list != ite_list)
 	{
 		std::cout << *it_list << std::endl;
 		++it_list;
 	}
-	std::list<int> s(list_test);
 
 	return 0;
 }
