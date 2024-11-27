333314c3233e9d0de987a246688567e8eeda964f
LegendreMatisse <legendre.matisse@gmail.com>
2024-11-27 13:31:07 +0100

    Added files and added includes

---
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+
+class BitcoinExchange {
+
+};
+
+
+
+#endif //BITCOINEXCHANGE_HPP
