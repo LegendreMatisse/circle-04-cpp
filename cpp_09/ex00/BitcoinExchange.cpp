c19c31677593b3c54eebd5ce49e568694d1a9991
LegendreMatisse <legendre.matisse@gmail.com>
2025-01-26 17:31:33 +0100

    Update BitcoinExchange.cpp

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -85,7 +85,7 @@ void BitcoinExchange::_fillExchangeRateMapWithDateAndRate(std::ifstream &file)
 		rate = line.substr(line.find(',') + 1);
 		_checkRate(rate);
 
-		_exchangeRate[date] = std::strtof(rate.c_str());
+		_exchangeRate[date] = std::strtof(rate.c_str(), NULL);
 
 		std::cout << date << ":" << rate << std::endl;
 	}
