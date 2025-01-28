81ae672521296c26abb773cf5658a692a0f979a5
Matisse Legendre <matisse.legendre@persgroep.net>
2025-01-28 17:18:53 +0100

    Done with cpp, or so I hope

--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:33:56 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/09/25 18:33:56 by mlegendr         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:17:33 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,49 +42,5 @@ int main(int argc, char **argv)
 		std::cerr << e.what();
 	}
 	
-
 	return 0;
-
-	/*std::vector<int> v;
-	std::list<int> l;
-	int a[18];
-
-	std::vector<std::string> containerNames;
-
-	containerNames.push_back("vector");
-	containerNames.push_back("list");
-	containerNames.push_back("deque");
-
-	std::string containerName = typeid(v).name();
-
-	for (std::vector<std::string>::iterator it = containerNames.begin(); it != containerNames.end(); ++it)
-	{
-		if (containerName.find(*it) != std::string::npos)
-			std::cout << "The container type is: " << *it << std::endl;
-		else
-			std::cout << "This container type is not supported by this program." << std::endl;
-	}
-	
-	containerName = typeid(l).name();
-
-	for (std::vector<std::string>::iterator it = containerNames.begin(); it != containerNames.end(); ++it)
-	{
-		if (containerName.find(*it) != std::string::npos)
-			std::cout << "The container type is: " << *it << std::endl;
-		else
-			std::cout << "This container type is not supported by this program." << std::endl;
-	}
-
-	containerName = typeid(a).name();
-
-	for (std::vector<std::string>::iterator it = containerNames.begin(); it != containerNames.end(); ++it)
-	{
-		if (containerName.find(*it) != std::string::npos)
-			std::cout << "The container type is: " << *it << std::endl;
-		else
-			std::cout << "This container type is not supported by this program." << std::endl;
-	}
-
-	return 0;*/
-
 }
